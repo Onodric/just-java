2dabca9c8c6ba96dc677847070430cb5d9ef57bc
Belve D. Marks <belvedmarks4@gmail.com>
2017-08-12 15:49:55 -0500

    added more interactive buttons to manipulate quantity

--- app/src/main/java/com/belvedmarks4/justjava/MainActivity.java
+++ app/src/main/java/com/belvedmarks4/justjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.belvedmarks4.justjava;
 
+import android.icu.text.NumberFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +9,42 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Class variable for quantity
+    public int quantity = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display(quantity);
     }
 
     public void submitOrder(View view) {
-        display(77*2 + 1);
+        displayPrice(quantity*5);
+    }
+
+    public void quantityAdd(View view) {
+        quantity += 1;
+        display(quantity);
+    }
+
+    public void quantitySubtract(View view) {
+        if (quantity < 1) {
+            quantity = 0;
+        } else {
+            quantity -= 1;
+        }
+        display(quantity);
     }
 
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
 }
